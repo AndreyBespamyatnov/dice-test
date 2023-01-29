f9bff88a38f968420d7416cd2d6df343d8213ca9
Andrei Bespamiatnov <adminsgz@gmail.com>
2023-01-29 13:03:48 +0100

    Add sourceUrl

--- URLShortener.Ngx/src/environments/environment.development.ts
+++ URLShortener.Ngx/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    api: 'https://localhost:7242'
+    api: 'https://localhost:7242',
+    sourceUrl: 'https://github.com/AndreyBespamyatnov/dice-test'
 };
--- URLShortener.Ngx/src/environments/environment.ts
+++ URLShortener.Ngx/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    api: ''
+    api: '',
+    sourceUrl: 'https://github.com/AndreyBespamyatnov/dice-test'
 };
